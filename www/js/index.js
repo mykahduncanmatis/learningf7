0b1da16fbed850b3cc11ae5fd4be9be124e0de48
mykahduncanmatis <mykah.duncanmatis@uleth.ca>
2022-10-12 11:46:42 -0600

    added pages and started to customize

--- www/js/index.js
+++ www/js/index.js
@@ -2,5 +2,19 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+    routes: [
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page2/',
+        url: 'pages/page2.html',
+      },
+      {
+        path: '/page3/',
+        url: 'pages/page3.html',
+      }
+    ],
   });
 var mainView = app.views.create('.view-main')
